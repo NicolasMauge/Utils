67b5861186c861a78c880e91d137363de4546f65
Nicolas MAUGE <maugenicolas@gmail.com>
2018-07-29 20:54:52 +0200

    bug on results

--- colab_utils.py
+++ colab_utils.py
@@ -23,6 +23,29 @@ def upload(filename):
     
     print('title: %s, mimeType: %s' % (file_upload['title'], file_upload['mimeType']))
 
+def download_v2(filename, dest_file=None):
+    drive, gauth = googledrive_auth()
+
+    drive_service = build('drive', 'v3')
+    results = drive_service.files().list(
+            q="name = '{}'".format(filename), fields="files(id)").execute()
+    results_files = results.get('files', [])
+
+    if dest_file is not None:
+        filename_dest = dest_file + filename
+    else:
+        filename_dest = filename
+        
+    #filename = "/content/.kaggle/kaggle.json"
+    os.makedirs(os.path.dirname(filename_dest), exist_ok=True)
+    request = drive_service.files().get_media(fileId=results_files[0]['id'])
+    fh = io.FileIO(filename_dest, 'wb')
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    os.chmod(filename_dest, 600)
 
 def download(filename, dest_file=None):
     drive, gauth = googledrive_auth()
@@ -36,9 +59,9 @@ def download(filename, dest_file=None):
         filename_with_directory = os.path.join(local_download_path, filename)
     else:
         filename_with_directory = filename
-        
+
     #results = drive.files().list(q="name = '"+filename+"'", fields="files(id)").execute()
-    results = drive.ListFile({'q': "title = '%s' and trashed=false"%filename}).GetList()
+    results = drive.ListFile({'q': "title = '{}' and trashed=false".format(filename)}).GetList()
     #file_id = results.get('files', [])
     #file_id = results[0]['id']
 
