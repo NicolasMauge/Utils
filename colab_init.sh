336127d83e7c7c960aedfd904d8421c1d1040db9
Nicolas MAUGE <maugenicolas@gmail.com>
2018-07-30 12:48:35 +0200

    echos

--- colab_init.sh
+++ colab_init.sh
@@ -1,6 +1,10 @@
+echo Install of the PyTorch packages
 pip3 install -q torch
 pip install -q torchtext
-pip install -U -q PyDrive
 pip install -q torchvision
 
+echo Install of the PyDrive package
+pip install -U -q PyDrive
+
+echo Install of the kaggle package
 pip install -q kaggle
